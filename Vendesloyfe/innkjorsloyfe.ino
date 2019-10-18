884c27f4dd6df3846616840d9c3d5c18af7ea3e4
Aetesaki <aetesaki@sunomi.no>
2019-10-18 21:07:08 +0200

    Setup og settSignalTil function opprettet

--- Vendesloyfe/innkjorsloyfe.ino
+++ Vendesloyfe/innkjorsloyfe.ino
@@ -1,6 +1,6 @@
 // Sketch for innkjør til vendesløyfe
 // (c) Aetesaki 2019
-// version 0.1
+// version 0.1 (UTESTET)
 
 // pins
 // inputs
@@ -14,4 +14,52 @@ const int SIGNAL_STOPP = 6; // Utgang for å sette signal til stopp
 const int SIGNAL_KJOR = 7; // Utgang for å sette signal til kjør
 const int SIGNAL_LANGSOMT = 8; // Utgang for å sette signal til langsomt
 
+// constants
+const char SIGNALBILDE_AV = 0;
+const char SIGNALBILDE_STOP = 1;
+const char SIGNALBILDE_KJOR = 2;
+const char SIGNALBILDE_LANGSOMT = 3;
 
+void setup() {
+	// setup input pins
+	pinMode(INNKJ, INPUT_PULLUP);
+	pinMode(INNKJAV, INPUT_PULLUP);
+	pinMode(SPERRING, INPUT_PULLUP);
+	pinMode(RIVING, INPUT_PULLUP);
+	// setup output pins
+	pinMode(VEKSEL, OUTPUT);
+	pinMode(SIGNAL_KJOR, OUTPUT);
+	pinMode(SIGNAL_LANGSOMT, OUTPUT);
+	pinMode(SIGNAL_STOPP, OUTPUT);
+	// set outpins to default
+	digitalWrite(VEKSEL, FALSE); // sett veksel til usatt
+	settSignalTil(SIGNALBILDE_STOP); // sett signal til stopp
+}
+
+void settSignalTil(char bilde) {
+	switch (bilde) {
+	case SIGNALBILDE_STOP:
+		digitalWrite(SIGNAL_STOPP, HIGH);
+		digitalWrite(SIGNAL_KJOR, LOW);
+		digitalWrite(SIGNAL_LANGSOMT, LOW);
+		break;
+	case SIGNALBILDE_KJOR:
+		digitalWrite(SIGNAL_STOPP, LOW);
+		digitalWrite(SIGNAL_KJOR, HIGH);
+		digitalWrite(SIGNAL_LANGSOMT, LOW);
+		break;
+	case SIGNALBILDE_LANGSOMT:
+		digitalWrite(SIGNAL_STOPP, LOW);
+		digitalWrite(SIGNAL_KJOR, HIGH);
+		digitalWrite(SIGNAL_LANGSOMT, HIGH);
+		break;
+	default:
+		digitalWrite(SIGNAL_STOPP, LOW);
+		digitalWrite(SIGNAL_KJOR, LOW);
+		digitalWrite(SIGNAL_LANGSOMT, LOW);
+		break;
+	}
+}
+	
+	
+	
