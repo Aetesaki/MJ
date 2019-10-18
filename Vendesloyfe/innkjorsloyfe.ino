ab4d0710fd6f9b4b2ccb3b42e3f4421f63bd3636
Aetesaki <aetesaki@sunomi.no>
2019-10-18 20:43:55 +0200

    Sketch for Innkjør til Vendesløyfe påbegynt

---
+++ Vendesloyfe/innkjorsloyfe.ino
@@ -0,0 +1,17 @@
+// Sketch for innkjør til vendesløyfe
+// (c) Aetesaki 2019
+// version 0.1
+
+// pins
+// inputs
+const int INNKJ = 1; // Forespørsel om innkjør til vendesløyfe
+const int INNKJAV = 2; // – avvikende
+const int SPERRING = 3; // Sperring av innkjør til vendesløyfe
+const int RIVING = 4; // Signal for riving av signal
+// outputs
+const int VEKSEL = 5; // Utgang for å legge om veksel
+const int SIGNAL_STOPP = 6; // Utgang for å sette signal til stopp
+const int SIGNAL_KJOR = 7; // Utgang for å sette signal til kjør
+const int SIGNAL_LANGSOMT = 8; // Utgang for å sette signal til langsomt
+
+
