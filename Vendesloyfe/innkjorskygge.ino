de8811e70bf2e9f90e66bfd94287ac6a11caf24c
Aetesaki <aetesaki@sunomi.no>
2019-10-24 22:07:45 +0200

    Innkjør til skyggestasjon påbegynt

---
+++ Vendesloyfe/innkjorskygge.ino
@@ -0,0 +1,21 @@
+// Sketch for innkjør til vendesløyfe
+// (c) Aetesaki 2019
+// version 0.1 (UTESTET)
+
+// pins must be changed depending on your device
+// inputs 
+const int FORRIGLING_INN = 2 // inngang for forrigling fra togspor
+const int TOGSPOR_BESATT = 3 // inngang for fulle togspor
+// outputs
+const int FORRIGLING_UT	= 4 // utgang for forrigling av innkjør
+const int SIGNAL_SIMULERT = 13 // utgang for simulert signal
+
+
+
+void setup() {
+	
+}
+
+void loop() {
+	
+}
