b15a06489fec0a3768e92720a9bd70b4104d632f
Aetesaki <aetesaki@sunomi.no>
2019-10-27 18:44:26 +0100

    urb v2.5 track functions

--- URB/source/urb.cpp
+++ URB/source/urb.cpp
@@ -18,13 +18,29 @@
 #include <Servo.h>
 
 const byte defaultSpeedArray[] = {20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200,210,220,230,240,255};
+const byte DIRECTION_FORWARD = 1;
+const byte DIRECTION_BACKWARDS = 2;
 void(* resetFunc) (void) = 0;
+
+// arrays of L298 pins
 int _L298EnablePin[10];
 int _L298PinA[10];
 int _L298PinB[10];
 
+// temporary internal data variable
+byte _dataReceivedFromBus;
+
+// Internal function for receiving data from bus
+void receiveFromBus(int howMany) {
+	// While there are data available
+	while (Wire.available() > 0) {
+		// dump that data into the temporary data variable
+		_dataReceivedFromBus = Wire.read();
+	}
+}
+
 void urbclass::setup(void) {
-	// Initializing COMM
+	// Initializing bus as master
 	Wire.begin();
 	// Set PWM frequency for D9 & D10
 	// Timer 1 divisor to 256 for PWM frequency of 122.55 Hz
@@ -32,8 +48,10 @@ void urbclass::setup(void) {
 }
 
 void urbclass::setup(int address) {
-	// Initializing COMM
+	// Initializing bus as slave
 	Wire.begin(address);
+	// setting up bus interrupt to access internal function
+	Wire.onReceive(receiveFromBus);
 	// Set PWM frequency for D9 & D10
 	// Timer 1 divisor to 256 for PWM frequency of 122.55 Hz
 	TCCR1B = TCCR1B & B11111000 | B00000100;   
@@ -42,7 +60,7 @@ void urbclass::setup(int address) {
 void urbclass::initializeTrack(byte track, int enablePin, int  pinA, int pinB) {
 	// setting up track connection
 	byte local = track - 1;
-	// save pins internally
+	// save pins in internal array
 	_L298EnablePin[local] = enablePin;
 	_L298PinA[local] = pinA;
 	_L298PinB[local] = pinB;
@@ -53,6 +71,50 @@ void urbclass::initializeTrack(byte track, int enablePin, int  pinA, int pinB) {
 	// enabling defaults
 	digitalWrite(_L298PinA[local], HIGH);
 	digitalWrite(_L298PinB[local], LOW);
+}
+
+// convert internal temporary data to readable data
+byte urbclass::dataFromBus() {
+	if (_dataReceivedFromBus)
+		return _dataReceivedFromBus;
+}
+
+// send data to bus
+void urbclass::sendDataToBus(int address, byte data) {
+	// Transmit to address
+	Wire.beginTransmission(address);
+	// Send data
+	Wire.write(data);
+	// Close transmission
+	Wire.endTransmission();
+}
+
+// functions for controlling the tracks
+void urbclass::controlTrack(byte track, byte speed) {
+	// set the current PWM of track to speed
+	byte local = track - 1;
+	analogWrite(_L298EnablePin[local], speed);
+}
+
+void urbclass::stopTrack(byte track) {
+	controlTrack(track, 0);
+}
+
+void urbclass::reverseTrack(byte track, byte direction) {
+	stopTrack(track);
+	byte local = track - 1; 
+	switch (direction) {
+		case DIRECTION_FORWARD:
+			digitalWrite(_L298PinA[local], HIGH);
+			digitalWrite(_L298PinB[local], LOW);
+			break;
+		case DIRECTION_BACKWARDS:
+			digitalWrite(_L298PinA[local], LOW);
+			digitalWrite(_L298PinB[local], HIGH);
+			break;
+		default:
+			break;
+	}
 }
 
 urbClass URB;
--- URB/source/urb.h
+++ URB/source/urb.h
@@ -23,12 +23,20 @@ class urbClass {
 		void setupComm(void);
 		void setupComm(int);
 		void initializeTrack(byte,int,int,int);
+		void sendDataToBus(int, byte);
+		void controlTrack(byte, byte);
+		void stopTrack(byte);
+		void reverseTrack(byte, byte);
 		byte defaultSpeedArray[];
+		byte dataFromBus();
+		byte DIRECTION_FORWARD;
+		byte DIRECTION_BACKWARDS;
 	private:
 		// private functions and variables
 		int _L298EnablePin[10];
 		int _L298PinA[10];
 		int _L298PinB[10];
+		byte _dataReceivedFromBus;
 };
 
 extern urbClass URB;
