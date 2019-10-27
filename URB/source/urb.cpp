89df2a0e3988466efb82a1f1715dfd8b25d88273
Aetesaki <aetesaki@sunomi.no>
2019-10-27 15:01:12 +0100

    universal railway bus version 2.5

---
+++ URB/source/urb.cpp
@@ -0,0 +1,14 @@
+//-------------------------------------//
+//    WWW.ARDUINORAILWAYCONTROL.COM    //
+//-------------------------------------//
+//    urb.cpp                          //
+//    urb library                      //
+//    URB 2 FINAL                      //
+//    PROTOCOL 2.5                     //
+//    PWM 122 Hz                       //
+//    V.10.2019  Aetesaki              //
+//                                     //
+//    PROTOCOL 2.5                     //
+//    V.5.2019  Steve Massikker        //
+//-------------------------------------//
+
---
+++ URB/source/urb.h
@@ -0,0 +1,14 @@
+//-------------------------------------//
+//    WWW.ARDUINORAILWAYCONTROL.COM    //
+//-------------------------------------//
+//    urb.h                            //
+//    urb library header               //
+//    URB 2 FINAL                      //
+//    PROTOCOL 2.5                     //
+//    PWM 122 Hz                       //
+//    V.10.2019  Aetesaki              //
+//                                     //
+//    PROTOCOL 2.5                     //
+//    V.5.2019  Steve Massikker        //
+//-------------------------------------//
+
