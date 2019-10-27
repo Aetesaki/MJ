977a446ee0273b4ae87e489fd06c82244c9daa9d
Aetesaki <aetesaki@sunomi.no>
2019-10-27 16:38:58 +0100

    urb v2.5 setup functions

--- URB/source/urb.cpp
+++ URB/source/urb.cpp
@@ -18,3 +18,40 @@
 #include <Wire.h>
 #include <Servo.h>
 
+void(* resetFunc) (void) = 0;
+string _inputString = "";
+int _L298EnablePin[10];
+int _L298PinA[10];
+int _L298PinB[10];
+
+void urbclass::setupComm(void) {
+	// Initializing COMM
+	Wire.begin();
+	Serial.begin(9600);
+	_inputString.reserve(4);
+}
+
+void urbclass::setupComm(int address) {
+	// Initializing COMM
+	Wire.begin(address);
+	Serial.begin(9600);
+	_inputString.reserve(4);
+}
+
+void urbclass::setupTrack(byte track, int enablePin, int  pinA, int pinB) {
+	// setting up track connection
+	byte local = track - 1;
+	// save pins internally
+	_L298EnablePin[local] = enablePin;
+	_L298PinA[local] = pinA;
+	_L298PinB[local] = pinB;
+	// setting pins as OUTPUT
+	pinMode(_L298EnablePin[local], OUTPUT);
+	pinMode(_L298PinA[local], OUTPUT);
+	pinMode(_L298PinB[local], OUTPUT);
+	// enabling defaults
+	digitalWrite(_L298PinA[local], HIGH);
+	digitalWrite(_L298PinB[local], LOW);
+}
+
+urbClass URB;
--- URB/source/urb.h
+++ URB/source/urb.h
@@ -20,8 +20,15 @@
 class urbClass {
 	public:
 		// public functions and variables
+		void setupComm(void);
+		void setupComm(int);
+		void setupTrack(byte,int,int,int);
 	private:
 		// private functions and variables
+		string _inputString;
+		int _L298EnablePin[10];
+		int _L298PinA[10];
+		int _L298PinB[10];
 };
 
 extern urbClass URB;
